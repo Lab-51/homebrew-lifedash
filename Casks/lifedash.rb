abfb1b4b4202cce8322e490fa9118f294fe8686b
Daniel Rieger <65163792+Lab-51@users.noreply.github.com>
2026-04-03 16:55:44 +0200

    Bump to v2.2.34

--- Casks/lifedash.rb
+++ Casks/lifedash.rb
@@ -1,6 +1,6 @@
 cask "lifedash" do
-  version "2.2.33"
-  sha256 "101ccc48d669ccb38877bd6d310bdd803f9159e3281e3f056582a2256b90c7d5"
+  version "2.2.34"
+  sha256 "598845abbf32369f151363128dd7ea2ff68bcda994e8d818ee8c72a3377d98d6"
 
   url "https://github.com/Lab-51/lifedash/releases/download/v#{version}/LifeDash-#{version}-mac-arm64.dmg"
   name "LifeDash"
